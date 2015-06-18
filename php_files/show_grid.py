a5ffc6273785536eb715a5cfc7c4e796a6f3be79
Arpit <arpit@Arpits-Mac-mini.local>
2015-06-18 11:18:21 +0530

    Added python script for development purpose

---
+++ php_files/show_grid.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import requests
+import json
+
+hit_url = "http://192.168.1.91/wpg/gridgen.php?len=8&words=abhay,arpit,varun,sainyam,joel,aman,monk"
+grid_len = 8
+r = requests.get(hit_url)
+if r.status_code == requests.codes.ok :
+	url_data = json.loads(r.text)
+	for i in range(0,grid_len):
+		output_str = ""
+		for j in range(0, grid_len):
+			p = '(' + str(i) + ',' + str(j) + ')'
+			
+			if p in url_data["grid"]:
+				output_str += url_data["grid"][p]
+			else:
+				output_str += " "
+			output_str += " "
+		print output_str
+
+
